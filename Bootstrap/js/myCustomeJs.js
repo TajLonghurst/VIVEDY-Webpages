4bc5f9628cdc607c582158a8083451fc2b96226e
Tajinboxx <TajLonghurstsoft@gmail.com>
2021-08-04 01:32:39 +1200

    Added MoviesCards And Haft Hover

---
+++ Bootstrap/js/myCustomeJs.js
@@ -0,0 +1,110 @@
+
+//grabbing the ElementId and naming is CardPlus as found be for let word
+let CardPlus1 = document.getElementById("CardPlus1");
+//The "CardPlusHoverOn()" function is being called in the html
+function CardPlusHoverOn1() {
+    CardPlus1.style.cssText="width: 100%; border-radius: 2px;"
+}
+//this is the onmouseout
+function CardPlusHoverOff1() {
+    CardPlus1.style.cssText=" "
+}
+
+let CardPlus2 = document.getElementById("CardPlus2");
+function CardPlusHoverOn2() {
+    CardPlus2.style.cssText="width: 100%; border-radius: 2px;"
+}
+function CardPlusHoverOff2() {
+    CardPlus2.style.cssText=" "
+}
+
+let CardPlus3 = document.getElementById("CardPlus3");
+function CardPlusHoverOn3() {
+    CardPlus3.style.cssText="width: 100%; border-radius: 2px;"
+}
+function CardPlusHoverOff3() {
+    CardPlus3.style.cssText=" "
+}
+
+let CardPlus4 = document.getElementById("CardPlus4");
+function CardPlusHoverOn4() {
+    CardPlus4.style.cssText="width: 100%; border-radius: 2px;"
+}
+function CardPlusHoverOff4() {
+    CardPlus4.style.cssText=" "
+}
+let CardPlus5 = document.getElementById("CardPlus5");
+function CardPlusHoverOn5() {
+    CardPlus5.style.cssText="width: 100%; border-radius: 2px;"
+}
+function CardPlusHoverOff5() {
+    CardPlus5.style.cssText=" "
+}
+
+let CardPlus6 = document.getElementById("CardPlus6");
+function CardPlusHoverOn6() {
+    CardPlus6.style.cssText="width: 100%; border-radius: 2px;"
+}
+function CardPlusHoverOff6() {
+    CardPlus6.style.cssText=" "
+}
+
+let CardPlus7 = document.getElementById("CardPlus7");
+function CardPlusHoverOn7() {
+    CardPlus7.style.cssText="width: 100%; border-radius: 2px;"
+}
+function CardPlusHoverOff7() {
+    CardPlus7.style.cssText=" "
+}
+
+let CardPlus8 = document.getElementById("CardPlus8");
+function CardPlusHoverOn8() {
+    CardPlus8.style.cssText="width: 100%; border-radius: 2px;"
+}
+function CardPlusHoverOff8() {
+    CardPlus8.style.cssText=" "
+}
+
+let CardPlus9 = document.getElementById("CardPlus9");
+function CardPlusHoverOn9() {
+    CardPlus9.style.cssText="width: 100%; border-radius: 2px;"
+}
+function CardPlusHoverOff9() {
+    CardPlus9.style.cssText=" "
+}
+
+let CardPlus10 = document.getElementById("CardPlus10");
+function CardPlusHoverOn10() {
+    CardPlus10.style.cssText="width: 100%; border-radius: 2px;"
+}
+function CardPlusHoverOff10() {
+    CardPlus10.style.cssText=" "
+}
+
+let CardPlus11 = document.getElementById("CardPlus11");
+function CardPlusHoverOn11() {
+    CardPlus11.style.cssText="width: 100%; border-radius: 2px;"
+}
+function CardPlusHoverOff11() {
+    CardPlus11.style.cssText=" "
+}
+
+let CardPlus12 = document.getElementById("CardPlus12");
+function CardPlusHoverOn12() {
+    CardPlus12.style.cssText="width: 100%; border-radius: 2px;"
+}
+function CardPlusHoverOff12() {
+    CardPlus12.style.cssText=" "
+}
+
+for (let i = 0; Num < 12; i++) {
+
+    let CardPlus = document.getElementById("CardPlus" + Num);
+    function CardPlusHoverOn() {
+        CardPlus.style.cssText="width: 100%; border-radius: 2px;"
+    }
+    function CardPlusHoverOff() {
+        CardPlus.style.cssText=" "
+    }
+
+}
